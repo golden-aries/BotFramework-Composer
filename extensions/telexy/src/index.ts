cfbd00a6e01716294e35c1b8f15d8c3c34d10d76
Sergey Ermolenko <sergey.ermolenko@telexy.com>
2021-10-12 14:46:12 -0700

    moving runtime to telexy

--- extensions/telexy/src/index.ts
+++ extensions/telexy/src/index.ts
@@ -1,12 +1,14 @@
 import { IExtensionRegistration } from '@botframework-composer/types';
 import { TelexyStorage } from './storage/telexyStorage';
 import { TelexyPublisher } from './publish/telexyPublish';
+import initRuntimes from './runtimes/telexyRuntimes';
 
 async function initialize(registration: IExtensionRegistration): Promise<void> {
   registration.useStorage(TelexyStorage);
   const publisher = new TelexyPublisher(registration);
   // register this publishing method with Composer
   await registration.addPublishMethod(publisher);
+  await initRuntimes(registration);
 }
 module.exports = {
   initialize,
