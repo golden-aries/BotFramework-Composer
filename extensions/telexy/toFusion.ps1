9742549c140803d1b9df3f81b1f94d0ae3810691
Sergey Ermolenko <sergey.ermolenko@telexy.com>
2023-02-06 08:49:53 -0800

    copying to fusion comments

--- extensions/telexy/toFusion.ps1
+++ extensions/telexy/toFusion.ps1
@@ -20,3 +20,13 @@ if ($choice -eq "y") {
 } else {
     Write-Host "No is selected!" -ForegroundColor DarkCyan
 }
+
+# https://learn.microsoft.com/en-us/windows-server/administration/windows-commands/robocopy
+# /e	Copies subdirectories. This option automatically includes empty directories.
+# /purge	Deletes destination files and directories that no longer exist in the source. Using this option with the /e option and a destination directory, allows the destination directory security settings to not be overwritten.
+# /mir	Mirrors a directory tree (equivalent to /e plus /purge). Using this option with the /e option and a destination directory, overwrites the destination directory security settings.
+# /v	Produces verbose output, and shows all skipped files.
+# /xo	Source directory files older than the destination are excluded from the copy.
+# /xf <filename>[ ...]	Excludes files that match the specified names or paths. Wildcard characters (* and ?) are supported.
+# /xd <directory>[ ...]	Excludes directories that match the specified names and paths.
+# /x	Reports all extra files, not just the ones that are selected.
